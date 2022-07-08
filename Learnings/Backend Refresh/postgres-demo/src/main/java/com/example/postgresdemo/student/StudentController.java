e62ce08930c1f6f19000a037f591858d0d905f03
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-07-08 16:42:23 +0530

    Done with Postgres CRUD

--- Learnings/Backend Refresh/postgres-demo/src/main/java/com/example/postgresdemo/student/StudentController.java
+++ Learnings/Backend Refresh/postgres-demo/src/main/java/com/example/postgresdemo/student/StudentController.java
@@ -22,4 +22,17 @@ public class StudentController {
   public void registerStudent(@RequestBody Student student) {
     studentService.addNewStudent(student);
   }
+
+  @DeleteMapping(path = "{studentId}")
+  public void deleteStudent(@PathVariable("studentId") Long studentId) {
+    studentService.deleteStudent(studentId);
+  }
+
+  @PutMapping(path = "{studentId}")
+  public void updateStudentNameAndEmail(
+          @PathVariable("studentId") Long studentId,
+          @RequestParam(required = false) String name,
+          @RequestParam(required = false) String email) {
+      studentService.updateStudentNameAndEmail(studentId, name, email);
+  }
 }
--- Learnings/Backend Refresh/postgres-demo/src/main/java/com/example/postgresdemo/student/service/Impl/StudentServiceImpl.java
+++ Learnings/Backend Refresh/postgres-demo/src/main/java/com/example/postgresdemo/student/service/Impl/StudentServiceImpl.java
@@ -5,8 +5,10 @@ import com.example.postgresdemo.student.repository.StudentRepository;
 import com.example.postgresdemo.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -30,4 +32,42 @@ public class StudentServiceImpl implements StudentService {
     studentRepository.save(student);
     System.out.println(student);
   }
+
+  public void deleteStudent(Long studentId) {
+    boolean studentExists = studentRepository.existsById(studentId);
+
+    if (!studentExists) {
+      throw new IllegalStateException(
+              "Student with ID: " + studentId + " does not exists!!"
+      );
+    }
+    studentRepository.deleteById(studentId);
+  }
+
+  @Transactional
+  public void updateStudentNameAndEmail(Long studentId, String name, String email) {
+    Student student = studentRepository.findById(studentId)
+            .orElseThrow(() -> new IllegalStateException(
+                    "Student with ID " + studentId + " does not exists!! :("
+            ));
+    // check constraints for name
+    if (Objects.nonNull(name) &&
+        name.length() > 0 &&
+        !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+    // check for email
+    if (Objects.nonNull(email) &&
+        email.length() > 0 &&
+        !Objects.equals(email, student.getEmail())) {
+      // check if the email is already taken by someone else
+      Optional<Student> otherStudent = studentRepository.findStudentByEmail(email);
+      System.out.println("otherStudent.toString() = " + otherStudent.toString());
+      if (otherStudent.isPresent()) {
+        throw new IllegalStateException("Email already taken!!!");
+      }
+      student.setEmail(email);
+    }
+  }
+
 }
